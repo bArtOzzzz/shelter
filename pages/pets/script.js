ce68f72ec617a5f800b8e2ceee3c0eb16009455d
bArtOzzzz <breakyourbrain55@gmail.com>
2020-10-23 13:55:13 +0300

    asrfgbhjkSDY

---
+++ pages/pets/script.js
@@ -0,0 +1,32 @@
+let clicks = 1;
+
+function forwardClick() {
+  clicks++;
+  document.getElementById("page-number").innerHTML = clicks;
+};
+function forwardAll() {
+  clicks += 5;
+  document.getElementById("page-number").innerHTML = clicks;
+}
+function backAll() {
+  let buttons = document.querySelector('.back-all');
+  if (clicks < 5) {
+    buttons.setAttribute('aria-disabled', true);
+  } else if (clicks > 5) {
+    clicks -= 5;
+    document.getElementById("page-number").innerHTML = clicks;
+  }else if (clicks == 5) {
+    clicks -= 4;
+    buttons.setAttribute('aria-disabled', false);
+    document.getElementById("page-number").innerHTML = clicks;
+  }
+}
+function back() {
+  let buttons = document.querySelector('.back');
+  if (clicks <= 1) {
+    buttons.setAttribute('aria-disabled', true);
+  }else if (clicks > 1) {
+    clicks--;
+    document.getElementById("page-number").innerHTML = clicks;
+  }
+}
